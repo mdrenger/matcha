68c6d0dcae70de372f5457bd9b018b6f83fa0a84
md <md@darmstadt.ccc.de>
2017-05-13 09:49:43 +0200

    Service für stations und Shape Zone A

---
+++ api/run.py
@@ -0,0 +1,25 @@
+import json
+
+from flask import Flask, url_for
+app = Flask(__name__)
+
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+
+@app.route('/stations')
+def api_stations():
+    return json.dumps(json.load(open('data/stations.json')))
+
+@app.route('/shape/<shape>')
+def api_shape(shape):
+    a=[]
+    if shape == "A":
+     a= json.load(open('data/shapeA.json'))
+    return json.dumps(a)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
